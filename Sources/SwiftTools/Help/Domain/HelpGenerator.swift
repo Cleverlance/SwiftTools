922bd93527440f12d021ac50e7fca1c763e0e5ae
Jan Halousek <jan.halousek@cleverlance.com>
2022-03-29 07:54:32 +0200

    Add access to argument generating

--- Sources/SwiftTools/Help/Domain/HelpGenerator.swift
+++ Sources/SwiftTools/Help/Domain/HelpGenerator.swift
@@ -7,16 +7,17 @@
 
 public protocol HelpGenerator {
     func makeHelp(for cliInteractor: CLIInteractor) -> String
+    func makeHelp(for argument: ShellArgumentHelp) -> String
 }
 
 final class HelpGeneratorImpl: HelpGenerator {
     func makeHelp(for cliInteractor: CLIInteractor) -> String {
         let title = "\(cliInteractor.getKeyword()): \(cliInteractor.getDescription())"
-        let arguments = cliInteractor.getArguments().map(\.help).map(makeArgumentHelp(for:))
+        let arguments = cliInteractor.getArguments().map(\.help).map(makeHelp(for:))
         return ([title] + arguments).joined(separator: "\n")
     }
 
-    private func makeArgumentHelp(for argument: ShellArgumentHelp) -> String {
+    func makeHelp(for argument: ShellArgumentHelp) -> String {
         if argument.isOptional {
             return "  [--\(argument.name) \(argument.value)]  -  \(argument.description)"
         } else {
