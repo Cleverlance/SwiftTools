18d4dff4195389a29f641ade1de201572bb3a1c8
Jan Halousek <jan.halousek@cleverlance.com>
2022-03-29 15:27:59 +0200

    Add static assemblies getter

---
+++ Sources/SwiftTools/SwiftToolsAssemblies.swift
@@ -0,0 +1,32 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jan Halousek on 29.03.2022.
+//
+
+import Swinject
+
+public enum SwiftToolsAssemblies {
+    public static func getAllAssemblies() -> [Assembly] {
+        return [
+            CleanupAssembly(),
+            FastlaneServiceAssembly(),
+            FullVersionAssembly(),
+            GitAssembly(),
+            HashServiceAssembly(),
+            LocalFileAssembly(),
+            NetworkAssembly(),
+            PrintAssembly(),
+            ShellAssembly(),
+            SlackAssembly(),
+            FormatAssembly(),
+            MergeAssembly(),
+            SynchronizeLatestReleaseAssembly(),
+            HelpGeneratorAssembly(),
+            ProjectAssembly(),
+            ShellArgumentsAssembly(),
+            UpdateAssembly(),
+        ]
+    }
+}
