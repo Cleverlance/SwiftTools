1ef0d600c25d5477297c4d8dae744743abab3319
Matej Krystof <krystof_matej@kb.cz>
2026-04-17 00:28:33 +0200

    Fix dependencies

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Swinject/Swinject.git", from: "2.8.1"),
         .package(url: "https://github.com/Swinject/SwinjectAutoregistration", from: "2.8.1"),
-        .package(url: "https://github.com/raptorxcz/xcbeautify", revision: "f2204485ba19bb36c4e62f4245a7b6f41d7d687a"),
+        .package(url: "https://github.com/raptorxcz/xcbeautify", exact: "3.3.0"),
     ],
     targets: [
         .target(name: "SwiftTools", dependencies: [
